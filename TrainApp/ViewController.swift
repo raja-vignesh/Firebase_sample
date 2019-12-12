ac98f4070195d5b98e6e0c6d4559d44fba301f09
rajavignesh <rajavignesh.ashok@cognizant.com>
2019-12-12 16:01:05 +0000

    Initial Commit

---
+++ TrainApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrainApp
+//
+//  Created by Ashok, Raja Vignesh (Cognizant) on 12/12/2019.
+//  Copyright © 2019 Ashok, Raja Vignesh (Cognizant). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
